f7c7065c0e3b9ca4b54d4962103dd6c8715efdcf
Clinton Manakane <clintonwamanakane@gmail.com>
2023-11-08 18:38:32 +0200

    init project

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
+    secret: env("ADMIN_JWT_SECRET", "b4dbad35-6caa-4f84-be56-90d0d3bc2fdf"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "d9b0df66ff97a666027e665707b4e3e7"),
+  },
+  transfer: {
+    token: {
+      salt: "d9b0df66ff97a666027e665707b4e3e7ssddds",
+    },
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,26 @@
-module.exports = ({ env }) => ({
+/* module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS')
+  },
+}); */
+
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "d9b0df66ff97a666027e665707b4e3e7"),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "77b2c87dbab4e1697bec244226fbd1b3"),
+    },
   },
 });
